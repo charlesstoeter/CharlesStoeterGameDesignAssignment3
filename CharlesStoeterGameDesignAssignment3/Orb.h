c790ac5f4152d8d257c2da155af96473fc2eea42
Charles Stoeter <charlesstoeter@gmail.com>
2025-06-08 18:11:31 -0400

    added Orb.h (projectile)

---
+++ CharlesStoeterGameDesignAssignment3/Orb.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef ORB_H
+#define ORB_H
+
+#include <allegro5/allegro.h>
+
+class Orb {
+public:
+    Orb();  // Constructor
+    void fire(int startX, int startY, float angle, ALLEGRO_BITMAP* img);
+    void update();
+    void draw();
+    void deactivate();
+    bool isLive() const;
+
+private:
+    int x, y;
+    float radianAngle;
+    int speed;
+    bool live;
+    ALLEGRO_BITMAP* image;
+};
+
+#endif
