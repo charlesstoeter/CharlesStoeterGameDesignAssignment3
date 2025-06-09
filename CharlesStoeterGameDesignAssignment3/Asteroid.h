9730e6443f28f83a84743f1e6df681a4ef569a7e
Charles Stoeter <charlesstoeter@gmail.com>
2025-06-08 22:06:07 -0400

    added asteroid.h

---
+++ CharlesStoeterGameDesignAssignment3/Asteroid.h
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef ASTEROID_H
+#define ASTEROID_H
+
+#include <allegro5/allegro.h>
+
+class Asteroid {
+public:
+    Asteroid();
+    ~Asteroid();
+
+    void start(int screenW);
+    void update();
+    void draw();
+    void collideWithGround();  // For iceberg collision if needed
+
+    void setLive(bool l);
+    bool isLive() const;
+
+    int getX() const;
+    int getY() const;
+    int getBoundX() const;
+    int getBoundY() const;
+
+private:
+    int x, y;
+    int speed;
+    int boundX, boundY;
+    bool live;
+    ALLEGRO_BITMAP* image;
+};
+
+#endif
